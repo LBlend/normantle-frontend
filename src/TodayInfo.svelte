81131777bedde0d799f445f017c15ab99ae62c3b
LBlend <lblend@protonmail.com>
2022-05-24 03:33:19 +0200

    initial todayinfo component

---
+++ src/TodayInfo.svelte
@@ -0,0 +1,13 @@
+<script>
+    let puzzleNumber = 0;
+    let similarity = 0;
+    let similarityTenth = 0;
+    let similarityThousandth = 0;
+</script>
+
+<div>
+    <p>Dagens ord er #{puzzleNumber}. Det nærmeste ordet har en likhetsgrad på {similarity}. Det tiende nærmeste har en likhetsgrad på {similarityTenth} og det tusende nærmeste ordet har en likhetsgrad på {similarityThousandth}.</p>
+</div>
+
+<style>
+</style>
