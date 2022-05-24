b69982738a519ee1dc1ad2bd6b9b4526be202f64
LBlend <lblend@protonmail.com>
2022-05-24 03:52:46 +0200

    initial FAQ

---
+++ src/Faq.svelte
@@ -0,0 +1,12 @@
+<div>
+	<h2>Spørsmål du kanskje har</h2>
+    
+    <h3>Når resettes klokka?</h3>
+    <p>Klokken 00:00 norsk tid</p>
+
+    <h3>Går det an å skrive på nynorsk?</h3>
+    <p>Nei, det går dessverre ikke. Både fordi jeg er lat og fordi jeg har et personlig vendetta mot nynorsk</p>
+
+    <h3>Spillet suger og har masse feil i seg!</h3>
+    <p>Da anbefaler jeg deg å rapportere om feilen på Github eller enda bedre, fikse feilen selv der.</p>
+</div>
