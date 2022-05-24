de9387a5d66ec68d1c6dc2fb1d106f90c871c2c6
LBlend <lblend@protonmail.com>
2022-05-24 04:14:43 +0200

    for some reason svelte does not like my file names. lazy commit then

--- src/Guess.svelte
+++ src/Guess.svelte
@@ -1,16 +1,58 @@
 <script lang="ts">
 	import Result from "./Result.svelte";
+
+    let hasGuessed = false;
+
+    let puzzleNumber = 0; // TODO
+    
+    async function onWordSubmit(e) {
+        const form = new FormData(e.target);
+        const word = form.get("word");
+
+        const response = await fetch("/guess", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                guess: word,
+                puzzleNumber: puzzleNumber
+            })
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            const { word, similarity, isClose } = data;  // TODO: make type
+
+            // TODO: update state. render result component
+        } else {
+            // TODO: show error message
+        }
+    }
+
+    async function onHintSubmit() {
+        // TODO
+    }
+
+    async function onSurrender() {
+        // TODO
+    }
 </script>
 
 <div>
     <form>
-        <input type="text">
+        <input type="text" id="word" name="ord">
         <input type="submit" value="Gjett">
     </form>
     <Result />
     <form>
         <input type="submit" value="Hint">
     </form>
+    {#if hasGuessed}
+    <form>
+        <input type="submit" value="Gi opp">
+    </form>
+    {/if}
 </div>
 
 
