78de09893ac9aa9b5a4e8d9c328f206973c0788d
LBlend <lblend@protonmail.com>
2022-05-24 04:40:06 +0200

    add spacing between questions

--- src/Faq.svelte
+++ src/Faq.svelte
@@ -1,18 +1,30 @@
 <div id="faq">
 	<h2>Spørsmål du kanskje har</h2>
 
-    <h3>Når resettes klokka?</h3>
-    <p>Klokken 00:00 norsk tid</p>
+    <div id="questions">
+        <div>
+            <h3>Når resettes klokka?</h3>
+            <p>Klokken 00:00 norsk tid</p>
+        </div>
 
-    <h3>Går det an å skrive på nynorsk?</h3>
-    <p>Nei, det går dessverre ikke. Både fordi jeg er lat og fordi jeg har et personlig vendetta mot nynorsk</p>
+        <div>
+            <h3>Går det an å skrive på nynorsk?</h3>
+            <p>Nei, det går dessverre ikke. Både fordi jeg er lat og fordi jeg har et personlig vendetta mot nynorsk</p>
+        </div>
 
-    <h3>Spillet suger og har masse feil i seg!</h3>
-    <p>Da anbefaler jeg deg å rapportere om feilen på Github eller enda bedre, fikse feilen selv der.</p>
+        <div>
+            <h3>Spillet suger og har masse feil i seg!</h3>
+            <p>Da anbefaler jeg deg å rapportere om feilen på Github eller enda bedre, fikse feilen selv der.</p>
+        </div>
+    </div>
 </div>
 
 <style>
     #faq {
-        margin-top: 2em;
+        margin-top: 2rem;
+    }
+    #questions {
+        display: grid;
+        gap: 1em;
     }
 </style>
