ab86d045a4c5a64a9184d4ea55c19f3b37415bea
LBlend <lblend@protonmail.com>
2022-05-25 00:36:24 +0200

    I don't even know anymore

--- src/Guess.svelte
+++ src/Guess.svelte
@@ -1,35 +1,46 @@
 <script lang="ts">
 	import Result from "./Result.svelte";
+    
+    //let apiRoot = "https://normantle-api.lblend.moe";  // TODO: move to .env
+    let apiRoot = "http://127.0.0.1:8000";  // TODO: move to .env
 
     let hasGuessed = false;
+    let puzzleNumber = 420;
 
-    let puzzleNumber = 0; // TODO
+    let guesses = [];
+
+    interface GuessResult {
+        word: string;
+        similarity: number;
+        isClose: boolean;
+        isCorrect: boolean;
+    }
     
     async function onWordSubmit(e) {
-        const form = new FormData(e.target);
-        const word = form.get("word");
+        const word = e.target.word.value;
 
         if (word === "") {
             return;
         }
 
-        const response = await fetch("/guess", {
+        const response = await fetch(`${apiRoot}/guess`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                guess: word,
+                word: word,
                 puzzleNumber: puzzleNumber
             })
         });
 
         if (response.ok) {
-            const data = await response.json();
-            const { word, similarity, isClose } = data;  // TODO: make type
-
-            // TODO: update state. render result component
+            const guessResponse: GuessResult = await response.json();
+            console.log("OK!", guessResponse);
+            const guessResult = { ...guessResponse, guessNumber: guesses.length + 1 };
+            guesses = [guessResult, ...guesses];
         } else {
+            console.log("NOT OK!", response);
             // TODO: show error message
         }
     }
@@ -48,7 +59,9 @@
         <input class="guessField" type="text" id="word" name="ord" placeholder="Gjett">
         <input class="guessButton" type="submit" value="Gjett">
     </form>
-    <Result />
+
+    <Result bind:guesses />
+
     {#if hasGuessed}
     <form on:submit|preventDefault={onHintSubmit}>
         <input type="submit" value="Hint">
@@ -62,18 +75,19 @@
 
 <style>
     #guess {
-        width: 100%;
-        display: flex;
+        display: grid;
+        gap: 1rem;
     }
     .guessForm {
-        flex: 1;
+        width: 100%;
+        display: flex;
     }
     .guessField {
         font-size: 1.5rem;
         width: 85%;
     }
     .guessButton {
-        width: 14%;
+        flex: 1;
         font-size: 1.25rem;
         background-color: #666666;
         color: #fff;
