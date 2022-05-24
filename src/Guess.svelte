42d4c35ea162d24cd9d20c926e7a3bb327d0c51b
LBlend <lblend@protonmail.com>
2022-05-24 03:30:27 +0200

    initial guess component

---
+++ src/Guess.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Result from "./Result.svelte";
+</script>
+
+<div>
+    <form>
+        <input type="text">
+        <input type="submit" value="Gjett">
+    </form>
+    <Result />
+    <form>
+        <input type="submit" value="Hint">
+    </form>
+</div>
+
+
+<style>
+
+</style>
