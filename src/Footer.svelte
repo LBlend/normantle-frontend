ccfd0a0213af62fe0bd583214358a91619360979
LBlend <lblend@protonmail.com>
2022-05-25 02:44:21 +0200

    rephrase footer text

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,6 +1,6 @@
 <footer>
-	<p>Normantle er en norsk kopi av David Turner sitt spill <a href="https://semantle.com/">Semantle</a></p>
-    <p>Laget av <a href="https://github.com/LBlend">LBlend</a> med Word embeddings fra <a href="https://www.mn.uio.no/ifi/english/research/groups/ltg/">LTG Oslo</a></p>
+	<p>Normantle er en norsk kopi av David Turners <a href="https://semantle.com/">Semantle</a></p>
+    <p>Laget av <a href="https://github.com/LBlend">LBlend</a> med word embeddings fra <a href="https://www.mn.uio.no/ifi/english/research/groups/ltg/">LTG Oslo</a></p>
 </footer>
 
 <style>
