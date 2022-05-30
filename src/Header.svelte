3e305e64696fd19a8fb5ff099f605e4255db65dc
LBlend <lblend@protonmail.com>
2022-05-30 15:49:47 +0200

    stylize header text

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,4 +1,23 @@
 <header>
-  <h1>Normantle</h1>
+  <h1>
+    <span class="red">N</span><span class="white">o</span><span class="blue"
+      >r</span
+    >mantle
+  </h1>
   <p>Semantle, på norsk!</p>
 </header>
+
+<style>
+  h1 {
+    color: lightblue;
+  }
+  .red {
+    color: #b5102d;
+  }
+  .white {
+    color: white;
+  }
+  .blue {
+    color: blue;
+  }
+</style>
